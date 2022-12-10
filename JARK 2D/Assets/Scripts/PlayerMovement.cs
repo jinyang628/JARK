6661b78018bf54e4d232aad45c96fc8ff4d48d84
R0yden <113604622+R0yden@users.noreply.github.com>
2022-12-10 16:41:10 +0800

    Movement Script

--- JARK 2D/Assets/Scripts/PlayerMovement.cs
+++ JARK 2D/Assets/Scripts/PlayerMovement.cs
@@ -5,48 +5,51 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    //
-    public Rigidbody player;
-    [Header("Gameplay Settings")]
-    public float thrustStrength = 10f;
+    private float horizontal;
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 16f;
+    private bool isFacingRight = true;
 
-    private float currentFuel;
+    [SerializeField] private Rigidbody2D player;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
 
-    // Start is called before the first frame update 
-    void Start()
-    {
-        
-    }
-    // Update is called once per frame 
     void Update()
     {
-        // Check if there is sufficient fuel for one burst of all nozzles 
-        if (currentFuel > 3 * nozzleFuelConsumptionRate * Time.deltaTime)
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            Thrust();
+            player.velocity = new Vector2(player.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && player.velocity.y > 0f)
+        {
+            player.velocity = new Vector2(player.velocity.x, player.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        player.velocity = new Vector2(horizontal * speed, player.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
         }
     }
 
-    void Thrust()
-    {
-        // Forward and backward velocity 
-        if ((Input.GetKey(KeyCode.W)  Input.GetKey(KeyCode.UpArrow)) != (Input.GetKey(KeyCode.S)  Input.GetKey(KeyCode.DownArrow)))  
-        {
-            bool forward = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
-            player.AddForce(thrustStrength * (forward ? transform.forward : -transform.forward));
-        }
-        // Upwards and downwards velocity 
-        if (Input.GetKey(KeyCode.Space) != Input.GetKey(KeyCode.C))
-        {
-            // todo    
-            bool up = Input.GetKey(KeyCode.Space);
-            player.AddForce(thrustStrength * (up ? transform.up : -transform.up));
-        }
-        // Left and right velocity 
-        if ((Input.GetKey(KeyCode.A)  Input.GetKey(KeyCode.RightArrow))) 
-        {
-            // todo 
-            bool right = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
-            player.AddForce(thrustStrength * (right ? transform.right : -transform.right));
-        }
-    }
+}
