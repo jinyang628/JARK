68d660180d788266293662df76067bba28f06c32
alvinlim277 <alvinlim9815@gmail.com>
2022-12-10 17:52:26 +0800

    Final push

--- JARK 2D/Assets/Scripts/AWScript.cs
+++ JARK 2D/Assets/Scripts/AWScript.cs
@@ -10,12 +10,14 @@ public class AWScript : MonoBehaviour
     [SerializeField]
     private Vector2 BGPosition;
     
+    // Resets heart to centre of wheel
     public void Reset_Heart()
     {
         BGPosition = new Vector2(Background.transform.position.x, Background.transform.position.y);
         Heart.transform.position = BGPosition;
     }
 
+    // Stability changes are currently hard-coded...
     public void Increase_Stability()
     {
         StabilityZone.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f);
@@ -29,12 +31,15 @@ public class AWScript : MonoBehaviour
         }
             
     }
+    // Check if heart is in stability zone
+    public bool IsStable => Heart.GetComponent<Collider2D>().bounds.Intersects(StabilityZone.GetComponent<Collider2D>().bounds);
 
     void Start()
     {
         Reset_Heart();
     }
 
+    // For test scene, delete later
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -42,6 +47,11 @@ public class AWScript : MonoBehaviour
         Vector2 movement = new Vector2(horizontalInput, verticalInput);
         
         Heart.transform.Translate(movement * -6.0f * Time.deltaTime);
+        
+        if (IsStable)
+        {
+            Debug.Log("Stable");
+        }
     }
     
 }
