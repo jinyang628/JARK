196043896895aa87145786e8d30b52345ec6f48e
Jin Yang <81862152+jinyang628@users.noreply.github.com>
2022-12-10 16:51:16 +0800

    jy changes

--- JARK 2D/Assets/Scripts/ReturnToMainMenu.cs
+++ JARK 2D/Assets/Scripts/ReturnToMainMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class NewBehaviourScript : MonoBehaviour
+public class ReturnToMainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public void MainMenu(){
