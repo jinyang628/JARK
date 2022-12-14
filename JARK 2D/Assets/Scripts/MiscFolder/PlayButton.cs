5c0c24bdfd3e989bb8e354f81c52b85fc64b8062
R0yden <113604622+R0yden@users.noreply.github.com>
2022-12-14 15:47:06 +0800

    Added click sound to startmenu play button

--- JARK 2D/Assets/Scripts/MiscFolder/PlayButton.cs
+++ JARK 2D/Assets/Scripts/MiscFolder/PlayButton.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
+    public AudioSource clickAudio;
+    public AudioSource hoverAudio;
+
     public void LoadLevelSelection(){
         SceneManager.LoadScene("LevelSelection");
+    }
+
+
+    public void buttonClickSound()
+    {
+         clickAudio.Play();
+    }
+
+    public void buttonHoverSound()
+    {
+        hoverAudio.Play();
     }
 }
     
