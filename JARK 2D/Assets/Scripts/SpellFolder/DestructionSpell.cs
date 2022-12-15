6445d1d6ae95cc1f46d56f7979e34a6fd86eb1c9
Kyriel Abad <abadkyriel@gmail.com>
2022-12-15 20:25:23 +0800

    implemented functional destruction spell

--- JARK 2D/Assets/Scripts/SpellFolder/DestructionSpell.cs
+++ JARK 2D/Assets/Scripts/SpellFolder/DestructionSpell.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu]
 public class DestructionSpell : Spell
 {
+    public float destructionDist = 5f;
     private AudioSource sound;
     private string directory = "DestroySpellSound";
     void Awake()
@@ -17,10 +18,35 @@ public class DestructionSpell : Spell
 
     public override void Activate(GameObject parent)
     {
+        GameObject ClosestTaggedObject(string tag)
+        {
+            GameObject[] gos;
+            gos = GameObject.FindGameObjectsWithTag(tag);
+            GameObject closest = null;
+            float distance = Mathf.Infinity;
+            Vector3 position = parent.transform.position;
+            foreach (GameObject go in gos)
+            {
+                Vector3 diff = go.transform.position - position;
+                float curDistance = diff.sqrMagnitude;
+                if (curDistance < distance)
+                {
+                    closest = go;
+                    distance = curDistance;
+                }
+            }
+            return closest;
+        }        
         if (!sound) {
             sound = GameObject.Find(directory).GetComponent<AudioSource>();
         }
         Debug.Log(Name);
-        sound.Play();
+        GameObject closest = ClosestTaggedObject("Destructible");
+        if (closest && (parent.transform.position - closest.transform.position).sqrMagnitude < destructionDist) {
+            sound.Play();
+            Destroy(closest);
+        } else {
+            Debug.Log("No destructible objects in range");
+        }
     }
 }
