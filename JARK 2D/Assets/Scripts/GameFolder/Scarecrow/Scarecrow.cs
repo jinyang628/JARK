a5f6e5dfe60b596f4a75ac66eafbfaa96ad722f9
Jin Yang <81862152+jinyang628@users.noreply.github.com>
2022-12-16 20:24:39 +0800

    minor edits to all the levels, added level background

--- JARK 2D/Assets/Scripts/GameFolder/Scarecrow/Scarecrow.cs
+++ JARK 2D/Assets/Scripts/GameFolder/Scarecrow/Scarecrow.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scarecrow : MonoBehaviour
 {
     public GameObject TempRestartWalls;
     public GameObject TempWalls;
     public Animator _animator;
+    public GameObject instructions2;
 
     private void OnTriggerEnter2D(Collider2D col){
         if (col.gameObject.name == "Player"){
             Destroy(TempRestartWalls);
             Destroy(TempWalls);
             _animator.SetTrigger("OnScarecrowCollision");
+            StartCoroutine(Wait());
         }
+    }
+
+    IEnumerator Wait(){
+        yield return new WaitForSeconds(2);
+        instructions2.SetActive(true);  
     }
 }
