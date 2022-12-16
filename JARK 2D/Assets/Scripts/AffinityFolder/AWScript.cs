bc50141ec966b66bd0dd22d2d0f188a19fa10e76
alvinlim277 <alvinlim9815@gmail.com>
2022-12-16 18:05:23 +0800

    Update AWScript.cs

--- JARK 2D/Assets/Scripts/AffinityFolder/AWScript.cs
+++ JARK 2D/Assets/Scripts/AffinityFolder/AWScript.cs
@@ -7,7 +7,7 @@ public class AWScript : MonoBehaviour
     public GameObject Heart;
     public GameObject Background;
     public GameObject StabilityZone;
-    public float displacementFactor = 0.5f;
+    public int CurrentQuadrant;
     private PlayerStats player;
 
     // Resets heart to centre of wheel
@@ -39,13 +39,37 @@ public class AWScript : MonoBehaviour
             
     }
 
+    public void PointerUpdate()
+    {
+        float x = Heart.transform.position.x - Background.transform.position.x;
+        float y = Heart.transform.position.y - Background.transform.position.y;
+        float displacement = (Heart.transform.position - Background.transform.position).magnitude;
+
+
+        if (x < 0 && y >= 0)
+        {
+            CurrentQuadrant = 1;
+        }
+        else if (x >= 0 && y >= 0)
+        {
+            CurrentQuadrant = 2;
+        }
+        else if (x >= 0 && y < 0)
+        {
+            CurrentQuadrant = 3;
+        }
+        else
+        {
+            CurrentQuadrant = 4;
+        }
+    }
     public bool Stability => (Heart.transform.position - Background.transform.position).magnitude <= StabilityZone.GetComponent<Collider2D>().bounds.size.x / 2;
 
     // Update pointer position according to PlayerStats
     private void UpdateWheel()
     {
         Vector3 currPosition = Background.transform.position;
-        Vector3 newPosition = currPosition + new Vector3(player.GetCurrAffinity().x * displacementFactor, player.GetCurrAffinity().y * displacementFactor, 0);
+        Vector3 newPosition = currPosition + new Vector3(player.GetCurrAffinity().x * 0.5f, player.GetCurrAffinity().y * 0.5f, 0);
         if ((newPosition - Background.transform.position).magnitude <= Background.GetComponent<Collider2D>().bounds.size.x / 2)
         {
             Heart.transform.position = newPosition;
