2cdab9f9dee1885847d1da209c7a4a3e3a66c4a5
Jin Yang <81862152+jinyang628@users.noreply.github.com>
2022-12-16 17:21:54 +0800

    level 14

--- JARK 2D/Assets/Scripts/MiscFolder/IntroScene.cs
+++ JARK 2D/Assets/Scripts/MiscFolder/IntroScene.cs
@@ -7,6 +7,11 @@ public class IntroScene : MonoBehaviour
 {
     void OnEnable()
     {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait(){
+        yield return new WaitForSeconds(2);
         int current = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadSceneAsync(current + 1);
     }
